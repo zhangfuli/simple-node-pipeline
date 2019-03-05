3cdc35229864979a7468c19408ea0cebcc9ccb40
zhangfuli <1171067930@qq.com>
2019-03-05 15:06:17 +0800

    no message

--- dop-web/src/pages/Pipeline/index.js
+++
@@ -1,6 +0,0 @@
-import Pipeline from './Pipeline';
-import PipelineInfo from './PipelineInfo/PipelineInfo'
-import PipelineProject from './PipelineProject/PipelineProject'
-import PipelineTest from './test/test'
-
-export {Pipeline, PipelineInfo, PipelineProject, PipelineTest};
